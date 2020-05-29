f986bb3bad483966e48a897499773a7bff325e2c
mark-yeatman <mark.x.yeatman@gmail.com>
2020-05-29 10:44:39 -0500

    Added in center of mass velocity function for compass gait COM phase portrait.

--- Analysis/Plotting/com_y_phase_portrait_2Link.m
+++ Analysis/Plotting/com_y_phase_portrait_2Link.m
@@ -3,14 +3,15 @@ com_pos_y = zeros(size(tout));
 com_vel_y = zeros(size(tout));
 for i=1:length(tout)
     temp_pos = COM_pos_func(xout(i,:)', params);
-    com_pos_y = temp_pos(2);
+    com_pos_y(i) = temp_pos(2);
     
     temp_vel = COM_vel_func(xout(i,:)', params);
-    com_vel_y = temp_vel(2);
+    com_vel_y(i) = temp_vel(2);
 end
 
 figure('Name','Positions','NumberTitle','off')
-plot(com_pos_y,com_vel_y)
+plot(com_vel_y,com_pos_y)
 title('CoM y phase portrait')
-xlabel('\dot{y}')
-ylabel('y')
+xlabel('$\dot{y}$','Interpreter','latex')
+ylabel('y','Interpreter','latex')
+grid on
--- Models/2LinkCompassGait/make2LinkCompassGaitFunctions.m
+++ Models/2LinkCompassGait/make2LinkCompassGaitFunctions.m
@@ -8,12 +8,11 @@ function make2LinkCompassGaitFunctions()
     configFolder = thisdir;%(1:idcs(end)-1);
 
     %Where to get the "mathematica" functions
-    SymFuncsPath = "C:\Users\mxy110230\Box Sync\Research\StandardizedSimCode\Derivations\ToMatlabOutputs\2LinkCompassGait";
+    SymFuncsPath = (strcat(pwd,'\Derivations\ToMatlabOutputs\2LinkCompassGait'));
     addpath(genpath(SymFuncsPath))
 
     %What to comment
-    comment = "Compass Gait Configuration file. Only needs state as inputs, parameters are already substituted in";
-    
+
     %Make readme for the model and config. 
     fid = fopen(strcat(configFolder,'\SimulationFunctions\info.txt'), 'wt' );
     fprintf( fid,'Current Configuration: CompassGait\nThis configuration is supposed to act like the compass gait model, by locking the knees on the 4 link biped. The mass parameters come from Spongs 2007 Passivity Based Control of Biped Walking. The mass parameters are baked into the simulation functions by makeMatlabFunctionsCompass.m ');
@@ -36,9 +35,9 @@ function make2LinkCompassGaitFunctions()
     
     folder = strcat(configFolder,'\SimulationFunctions\DynamicsFunctions\');
     status = mkdir(folder);
-    matlabFunction(M,   'File',strcat(folder,'M_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(Cmat,'File',strcat(folder,'C_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(G,   'File',strcat(folder,'G_func'),'Vars',{x,params},'Comments',comment)
+    matlabFunction(M,   'File',strcat(folder,'M_func'),'Vars',{x,params})
+    matlabFunction(Cmat,'File',strcat(folder,'C_func'),'Vars',{x,params})
+    matlabFunction(G,   'File',strcat(folder,'G_func'),'Vars',{x,params})
     matlabFunction(ddCoM,'File',strcat(folder,'COM_accel_func'),'Vars',{x,ac,params})
 
     ASSupp_matrix
@@ -51,23 +50,23 @@ function make2LinkCompassGaitFunctions()
     
     folder = strcat(configFolder,'\SimulationFunctions\ConstraintFunctions\');
     status = mkdir(folder);
-    matlabFunction(sym(Assupp),     'File',strcat(folder,'A_SSupp_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(Aswap,           'File',strcat(folder,'A_Swap_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(sym(Avhc),   'File',strcat(folder,'A_VHC_func'),'Vars',{x,params},'Comments',comment)
+    matlabFunction(sym(Assupp),     'File',strcat(folder,'A_SSupp_func'),'Vars',{x,params})
+    matlabFunction(Aswap,           'File',strcat(folder,'A_Swap_func'),'Vars',{x,params})
+    matlabFunction(sym(Avhc),   'File',strcat(folder,'A_VHC_func'),'Vars',{x,params})
 
-    matlabFunction(sym(Adotssupp),  'File',strcat(folder,'Adot_SSupp_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(Adotswap,        'File',strcat(folder,'Adot_Swap_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(sym(Adotvhc),'File',strcat(folder,'Adot_VHC_func'),'Vars',{x,params},'Comments',comment)
+    matlabFunction(sym(Adotssupp),  'File',strcat(folder,'Adot_SSupp_func'),'Vars',{x,params})
+    matlabFunction(Adotswap,        'File',strcat(folder,'Adot_Swap_func'),'Vars',{x,params})
+    matlabFunction(sym(Adotvhc),'File',strcat(folder,'Adot_VHC_func'),'Vars',{x,params})
 
-    matlabFunction(sym(vhc),   'File',strcat(folder,'VHC_func'),'Vars',{x,params},'Comments',comment)
+    matlabFunction(sym(vhc),   'File',strcat(folder,'VHC_func'),'Vars',{x,params})
     
     Kinetic_energy
     Potential_energy
 
     folder = strcat(configFolder,'\SimulationFunctions\EnergyFunctions\');
     status = mkdir(folder);
-    matlabFunction(Ke,'File',strcat(folder,'KE_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(Pe,'File',strcat(folder,'PE_func'),'Vars',{x,params},'Comments',comment)
+    matlabFunction(Ke,'File',strcat(folder,'KE_func'),'Vars',{x,params})
+    matlabFunction(Pe,'File',strcat(folder,'PE_func'),'Vars',{x,params})
     
     Foot_st_pos
     Hip_pos
@@ -76,25 +75,27 @@ function make2LinkCompassGaitFunctions()
     
     folder = strcat(configFolder,'\SimulationFunctions\KinematicFunctions\');
     status = mkdir(folder);
-    matlabFunction(posFst,  'File',strcat(folder,'Foot_St_pos_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(posH,    'File',strcat(folder,'Hip_pos_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(posFsw,  'File',strcat(folder,'Foot_Sw_pos_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(posCoM,  'File',strcat(folder,'COM_pos_func'),'Vars',{x,params},'Comments',comment)
+    matlabFunction(posFst,  'File',strcat(folder,'Foot_St_pos_func'),'Vars',{x,params})
+    matlabFunction(posH,    'File',strcat(folder,'Hip_pos_func'),'Vars',{x,params})
+    matlabFunction(posFsw,  'File',strcat(folder,'Foot_Sw_pos_func'),'Vars',{x,params})
+    matlabFunction(posCoM,  'File',strcat(folder,'COM_pos_func'),'Vars',{x,params})
     
     Foot_st_vel
     Hip_vel
     Foot_sw_vel
-
-    matlabFunction(velFst,  'File',strcat(folder,'Foot_St_vel_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(velH,    'File',strcat(folder,'Hip_vel_func'),'Vars',{x,params},'Comments',comment)
-    matlabFunction(velFsw,  'File',strcat(folder,'Foot_Sw_vel_func'),'Vars',{x,params},'Comments',comment)
+    COM_vel
+    
+    matlabFunction(velFst,  'File',strcat(folder,'Foot_St_vel_func'),'Vars',{x,params})
+    matlabFunction(velH,    'File',strcat(folder,'Hip_vel_func'),'Vars',{x,params})
+    matlabFunction(velFsw,  'File',strcat(folder,'Foot_Sw_vel_func'),'Vars',{x,params})
+    matlabFunction(dCoM,'File',strcat(folder,'COM_vel_func'),'Vars',{x,params})
 
 %     Foot_st_Jacobian
 %     Hip_Jacobian
 %     Foot_sw_Jacobian
 % 
-%     matlabFunction(sym(js0),  'File',strcat(folder,'Foot_St_Jacobian_func'),'Vars',{x,params},'Comments',comment)
-%     matlabFunction(sym(js2),    'File',strcat(folder,'Hip_Jacobian_func'),'Vars',{x,params},'Comments',comment)
-%     matlabFunction(sym(js4),  'File',strcat(folder,'Foot_Sw_Jacobian_func'),'Vars',{x,params},'Comments',comment)
+%     matlabFunction(sym(js0),  'File',strcat(folder,'Foot_St_Jacobian_func'),'Vars',{x,params})
+%     matlabFunction(sym(js2),    'File',strcat(folder,'Hip_Jacobian_func'),'Vars',{x,params})
+%     matlabFunction(sym(js4),  'File',strcat(folder,'Foot_Sw_Jacobian_func'),'Vars',{x,params})
 %     
 end
